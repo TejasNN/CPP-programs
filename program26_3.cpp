04e2f3768c1af6ca04a068fcdc1637744b0fb1af
TejasNN <tejasnagvekar90@gmail.com>
2023-10-05 15:32:23 +0530

    - CPP code added

---
+++ program26_3.cpp
@@ -0,0 +1,69 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//																			                                           
+//	Problem statement :	Write a program which checks whether 7th, 15th, 21st and 28th bit is ON or OFF. 
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+typedef int BOOL;
+typedef unsigned int UINT;
+
+#define TRUE 1
+#define FALSE 0
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Function name :		CheckBit
+//	Input :				unsigned integer
+//	Output :			boolean
+// 	Description :		Returns true if 7th, 15th, 21st and 28th bit of the input number is 1.  
+// 	Author :			Tejas Nandakumar Nagvekar
+// 	Date :				06/06/2023
+// 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+BOOL CheckBit(UINT iNo)
+{
+    UINT iMask = 0X08104040;
+    UINT iResult = 0;
+
+    iResult = iNo & iMask;
+
+    if(iResult == iMask)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+//	Entry point function													   //
+/////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    UINT iValue = 0;
+    BOOL bRet = FALSE;
+
+    cout<<"Enter a number :"<<"\n";
+    cin>>iValue;
+
+    bRet = CheckBit(iValue);
+
+    if(bRet == TRUE)
+    {
+        cout<<"7th, 15th, 21st and 28th bit is ON"<<"\n";
+    }
+    else
+    {
+        cout<<"7th, 15th, 21st and 28th is OFF"<<"\n";
+    }
+
+    return 0;
+}
+// Time complexity : NA
